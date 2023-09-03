e09e04010677e77ee885ad40bd46067ef222f2de
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-09-03 16:57:33 -0600

    Add: menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+import styles from '@/styles/Menu/Menu.module.css'
+import { MenuItems } from '@/constants';
+import Link from 'next/link';
+
+const Menu = () => {
+  
+  const [expand, setExpand] = useState(false);
+
+  const setIcon = () => {
+    return expand ? "fas fa-times" : "fas fa-bars";
+
+  }
+
+  const handleClick = () => {
+    setExpand(!expand)
+  }
+
+  return (
+    <section className={styles.menuContainer}>
+      <section className={styles.iconContainer}>
+        <span
+          className={`${styles.icon} ${setIcon()}`}
+          onClick={handleClick}
+        ></span>
+      </section>
+
+      <ul className={styles.menu} data-expanded={expand}>
+        {MenuItems.map((item, index) =>
+          item.nodeType !== "button" ? (
+            <li key={index}>
+              <Link className={styles.menuItem} href={item.url}>
+                {item.name}
+              </Link>
+            </li>
+          ) : (
+            <li key={index}>
+              <button className={styles.button}>{item.name}</button>
+            </li>
+          )
+        )}
+      </ul>
+    </section>
+  );
+}
+
+export default Menu
