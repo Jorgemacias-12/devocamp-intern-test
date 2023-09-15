1b5ff5435cb61502b23de06d1ab4b6d1fe0be53b
Jorge Macias <63885983+Jorgemacias-12@users.noreply.github.com>
2023-09-15 12:59:34 -0600

    Develop (#5)
    * Remove: styles generated by next creation utility
    
    * Add: es-MX lang attr to Html tag
    
    * Remove: content generated by nextjs tool
    
    * Add: generate Layout component
    
    	And generate footer and header component that is going to be
    	used by Layout component.
    
    * Add: layout.module.css
    
    	And remove Home.module.css
    
    * Add: include layout in the route component
    
    * Add: accept any host images

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react'
+
+import styles from '@/styles/Layout/Layout.module.css'
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+
+type LayoutProps = {
+  children: ReactNode;
+}
+
+const Layout = ( {children}: LayoutProps) => {
+  return (
+    <section className={styles.appContainer}>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </section>
+  )
+}
+
+export default Layout
